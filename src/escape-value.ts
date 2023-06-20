f1229d07d38b6c55dcfcaac20989f8f55f95517e
Mihael Safaric <mihael.safaric@gmail.com>
2023-06-20 13:27:12 +0200

    Clean the escapeValue function

--- src/escape-value.ts
+++ src/escape-value.ts
@@ -1,13 +1,12 @@
 const CHARS_TO_ESCAPE = /["'();,=!~<>\s]/;
 
 export function escapeValue(value: unknown): string {
-  let valueString: string;
-
   if (Array.isArray(value)) {
     return `${value.map(escapeValue)}`;
-  } else {
-    valueString = value.toString();
   }
+
+  let valueString = value.toString();
+
   if (CHARS_TO_ESCAPE.test(valueString) || valueString.length === 0) {
     valueString = `"${valueString.replace(/"/g, '\\"')}"`;
   }
